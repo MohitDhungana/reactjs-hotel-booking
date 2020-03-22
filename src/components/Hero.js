d6b26b183d8f6b36cb3dff21150d4353801206e5
MohitDhungana <mohitdhungana@yahoo.com>
2020-03-22 20:58:21 +0545

    added hero and services component

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-export default function Hero({ chilren, hero }) {
-  return <header className={hero}>{chilren}</header>;
+export default function Hero({ children, hero }) {
+  return <header className={hero}>{children}</header>;
 }
 
 Hero.defaultProps = {
